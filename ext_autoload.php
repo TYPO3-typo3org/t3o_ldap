883c7ce3e0cc4b1907040c70d2394fac2ba073d5
Thomas Löffler <loeffler@spooner-web.de>
2016-11-11 23:01:04 +0100

    Update ext_autoload.php
    Fixes wrong class names

--- ext_autoload.php
+++ ext_autoload.php
@@ -3,9 +3,9 @@
  * Register necessary class names with autoloader
  */
 return array(
-    'tx_t3o_ldap_connectors_ldap' => t3lib_extMgm::extPath('t3o_ldap', 'Classes/Connectors/LDAP.php'),
-    'tx_t3o_ldap_utility_passwordhashing' => t3lib_extMgm::extPath('t3o_ldap', 'Classes/Utility/PasswordHashing.php'),
-    'tx_t3o_ldap_utility_passwordupdate' => t3lib_extMgm::extPath('t3o_ldap', 'Classes/Utility/PasswordUpdate.php'),
+    'tx_t3oldap_connectors_ldap' => t3lib_extMgm::extPath('t3o_ldap', 'Classes/Connectors/LDAP.php'),
+    'tx_t3oldap_utility_passwordhashing' => t3lib_extMgm::extPath('t3o_ldap', 'Classes/Utility/PasswordHashing.php'),
+    'tx_t3oldap_utility_passwordupdate' => t3lib_extMgm::extPath('t3o_ldap', 'Classes/Utility/PasswordUpdate.php'),
     'tx_t3oldap_utility_usercreateupdatedelete' => t3lib_extMgm::extPath('t3o_ldap', 'Classes/Utility/UserCreateUpdateDelete.php'),
     'tx_t3oldap_hooks_datahandlerhook' => t3lib_extMgm::extPath('t3o_ldap', 'Classes/Hooks/DataHandlerHook.php'),
 );
